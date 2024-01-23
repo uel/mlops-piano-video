e11b29287051788f8f306a7be4cb7aaf2cac7b89
uel <danifilip@seznam.cz>
2024-01-23 22:39:33 +0100

    bigger model

--- piano_video/train_model.py
+++ piano_video/train_model.py
@@ -33,29 +33,29 @@ def main(cfg):
 
     if os.path.exists(os.path.join("models", model_dir, "model.pt")):
         imagen = torch.load(os.path.join("models", model_dir, "model.pt"))
-        if torch.cuda.is_available():
-            imagen.cuda()
         print("Loaded model from file.")
     else:
         # unets for unconditional imagen
         unet = Unet(
-            dim = 32,
+            dim = 64,
             dim_mults = (1, 2, 4, 8),
             num_resnet_blocks = 1,
-            layer_attns = (False, False, False, True),
-            layer_cross_attns = False,
+            layer_attns = (False, True, True, True),
+            layer_cross_attns = (False, True, True, True),
             text_embed_dim=126,
             cond_dim=126
         )
 
         # imagen, which contains the unet above
-
         imagen = Imagen(
             unets = unet,
             image_sizes = 128,
             timesteps = 1000,
             text_embed_dim=126
         )
+
+    if torch.cuda.is_available():
+        imagen.cuda()
 
     # setting up the trainer class in denoising_diffusion_pytorch
     trainer = ImagenTrainer(
@@ -65,22 +65,26 @@ def main(cfg):
 
     dataset = Dataset(dataset_folder, image_size = 128)
 
-    trainer.add_train_dataset(dataset, batch_size = 16)
+    trainer.add_train_dataset(dataset, batch_size = 32)
 
-    for i in range(1000):
+    validation_landmarks = None
+
+    for i in range(10000):
         loss = trainer.train_step(unet_number = 1, max_batch_size = 4)
         print(f'loss/train: {loss}')
         writer.add_scalar('loss/train', loss, i) 
         run.log({"loss/train": loss})
 
-        if i and not (i % 50):
+        if i and not (i % 100):
             valid_loss = trainer.valid_step(unet_number = 1, max_batch_size = 4)
             print(f'valid loss: {valid_loss}')
             writer.add_scalar('loss/valid', valid_loss, i)
             run.log({"loss/valid": valid_loss})
 
-        if i and not (i % 100) and trainer.is_main: # is_main makes sure this can run in distributed
-            _, valid_landmarks = next(trainer.valid_dl_iter)
+        if i and not (i % 500) and trainer.is_main: # is_main makes sure this can run in distributed
+            if validation_landmarks is None:
+                _, valid_landmarks = next(trainer.valid_dl_iter)
+
             images = trainer.sample(text_embeds=valid_landmarks, batch_size = 1, return_pil_images = True)
             wandb.log({"samples": [wandb.Image(image) for image in images]})
             # images[0].save(f'./sample-{i // 100}.png')
